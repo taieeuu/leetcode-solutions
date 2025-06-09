2f9bd1f2cded6695f05bb3a98314f0ca8ea0d681
taieeuu <taieeuu@gmail.com>
2025-06-09 23:05:13 +0800

    add 953 solution
    Signed-off-by: taieeuu <taieeuu@gmail.com>

--- leetcode/953/953.py
+++ leetcode/953/953.py
@@ -1,3 +1,16 @@
 class Solution:
+		# Simulation
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        ...
+
+        order_dict = {val: idx for idx, val in enumerate(order)}
+        
+        def compare(word1, word2):
+            for x, y in zip(word1, word2):
+                if x != y:
+                    return order_dict[x] < order_dict[y]
+            return len(word1) <= len(word2)
+
+        for i in range(len(words)-1):
+            if not compare(words[i], words[i+1]):
+                return False
+        return True
